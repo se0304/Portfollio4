c9408c0996f34059344c7cc637ddc8f8d42571f6
se0304 <139088294+se0304@users.noreply.github.com>
2023-09-25 16:21:28 +0900

    6548915

--- js/sub.js
+++ js/sub.js
@@ -73,7 +73,7 @@ var book3;
 //텍스트 파일 1
 
 $(function(){
-    $.get("text/four.txt", function(data){
+    $.get("./text/four.txt", function(data){
         $(".textfour").html(data);
     })
 });
@@ -81,7 +81,7 @@ $(function(){
 //텍스트 파일 2
 
 $(function(){
-    $.get("text/five.txt", function(data){
+    $.get("./text/five.txt", function(data){
         $(".textfive").html(data);
     })
 });
@@ -89,13 +89,13 @@ $(function(){
 //텍스트 파일3
 
 $(function(){
-    $.get("text/seven.txt", function(data){
+    $.get("./text/seven.txt", function(data){
             $(".seventextbox").html(data);
     })
 });
 
 $(function(){
-    $.get("text/tb.txt", function(data){
+    $.get("./text/tb.txt", function(data){
         $(".td").html(data)
     })
 });
